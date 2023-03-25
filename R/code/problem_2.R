621b034ada163a435b14d7e053ba11c20a85b133
Ali Vafaee shahi <alivafa79@gmail.com>
2023-03-25 17:52:42 +0330

    Create problem_2.R

---
+++ R/code/problem_2.R
@@ -0,0 +1,18 @@
+FibToN <- function(n){
+  fib.vector <- c(1, 2)
+  while (fib.vector[length(fib.vector)] < n) {
+    new.number <- (fib.vector[length(fib.vector)]) + (fib.vector[length(fib.vector) - 1])
+    fib.vector <- c(fib.vector, new.number)
+  }
+  fib.vector <- fib.vector[-length(fib.vector)]
+}
+
+fibonacci.vector <- FibToN(4000000)
+fibonacci.vector.modified <- fibonacci.vector[-1]
+n.even <- ceiling(length(fibonacci.vector.modified) / 3)
+
+answer <- 0
+for (i in 0:(n.even - 1)) {
+  answer <- answer + fibonacci.vector.modified[(3 * i) + 1]
+}
+print(answer)
